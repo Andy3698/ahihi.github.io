3d43117e92a8cc49539bbb24780fb13a6515b754
Andy3698 <92646075+Andy3698@users.noreply.github.com>
2022-09-26 11:10:55 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: '<3',
+    introDesc: `<3 <3 <3 <3`,
+    btnIntro: 'Love You',
+    title: 'Ê Vân, e có thích anh không zạ',
+    desc: 'Chọn đi gái ơi , "below, below" ',
+    btnYes: 'Love love love',
+    btnNo: 'Không, Anh ơi :3',
+    question:'Vậy mà còn làm anh buôn chi zạ, xin lỗi anh đi',
+    btnReply: 'Gọi cho anh đi',
+    reply: 'Love you 3000',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Chiều nay 2h30 anh qua đưa em đi ăn Bún chả Hà Nội Phở Sơn nhé.',
+    btnAccept: 'Okeeeeeeee',
+    messLink: 'http://fb.com' 
+}
